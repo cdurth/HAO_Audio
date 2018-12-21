99a193acdab9b1bf498e96524e0bb61aef22a6aa
Corey Durthaler <cdurth@gmail.com>
2018-12-21 01:59:56 -0500

    initial commit

---
+++ AudioAnalyzer/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace AudioAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.progressBar6 = new System.Windows.Forms.ProgressBar();
+            this.progressBar7 = new System.Windows.Forms.ProgressBar();
+            this.progressBar8 = new System.Windows.Forms.ProgressBar();
+            this.progressBar9 = new System.Windows.Forms.ProgressBar();
+            this.progressBar10 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(23, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(233, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(23, 53);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(233, 23);
+            this.progressBar2.TabIndex = 1;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(23, 96);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(233, 23);
+            this.progressBar3.TabIndex = 2;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(23, 137);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(233, 23);
+            this.progressBar4.TabIndex = 3;
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(23, 179);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(233, 23);
+            this.progressBar5.TabIndex = 4;
+            // 
+            // progressBar6
+            // 
+            this.progressBar6.Location = new System.Drawing.Point(23, 222);
+            this.progressBar6.Name = "progressBar6";
+            this.progressBar6.Size = new System.Drawing.Size(233, 23);
+            this.progressBar6.TabIndex = 5;
+            // 
+            // progressBar7
+            // 
+            this.progressBar7.Location = new System.Drawing.Point(23, 263);
+            this.progressBar7.Name = "progressBar7";
+            this.progressBar7.Size = new System.Drawing.Size(233, 23);
+            this.progressBar7.TabIndex = 6;
+            // 
+            // progressBar8
+            // 
+            this.progressBar8.Location = new System.Drawing.Point(23, 303);
+            this.progressBar8.Name = "progressBar8";
+            this.progressBar8.Size = new System.Drawing.Size(233, 23);
+            this.progressBar8.TabIndex = 7;
+            // 
+            // progressBar9
+            // 
+            this.progressBar9.Location = new System.Drawing.Point(23, 344);
+            this.progressBar9.Name = "progressBar9";
+            this.progressBar9.Size = new System.Drawing.Size(233, 23);
+            this.progressBar9.TabIndex = 8;
+            // 
+            // progressBar10
+            // 
+            this.progressBar10.Location = new System.Drawing.Point(23, 386);
+            this.progressBar10.Name = "progressBar10";
+            this.progressBar10.Size = new System.Drawing.Size(233, 23);
+            this.progressBar10.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar10);
+            this.Controls.Add(this.progressBar9);
+            this.Controls.Add(this.progressBar8);
+            this.Controls.Add(this.progressBar7);
+            this.Controls.Add(this.progressBar6);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.ProgressBar progressBar6;
+        private System.Windows.Forms.ProgressBar progressBar7;
+        private System.Windows.Forms.ProgressBar progressBar8;
+        private System.Windows.Forms.ProgressBar progressBar9;
+        private System.Windows.Forms.ProgressBar progressBar10;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AudioAnalyzer/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Un4seen.Bass;
+using Un4seen.BassWasapi;
+
+namespace AudioAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        List<ProgressBar> bars = new List<ProgressBar>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Init();
+            StartAnalysis();
+        }
+
+        // BASS Process
+        private WASAPIPROC _WasapiProcess = new WASAPIPROC(Process);
+        private Thread _AnalysisThread;
+        private const int BANDS = 10;
+        private float[] _data = new float[512];
+        // Analysis settings
+        private int _SamplingRate = 44100;
+        private int _DeviceCode = 17;
+
+        // BASS initialization method
+        private void Init()
+        {
+            bool result = false;
+
+            foreach (Control control in this.Controls)
+            {
+                if (control.GetType() == typeof(ProgressBar))
+                {
+                    bars.Add((ProgressBar)control);
+                }
+            }
+
+            // Initialize BASS on default device
+            result = Bass.BASS_Init(0, _SamplingRate, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
+
+            if (!result)
+            {
+                throw new Exception(Bass.BASS_ErrorGetCode().ToString());
+            }
+
+            // Initialize WASAPI
+            result = BassWasapi.BASS_WASAPI_Init(_DeviceCode, 0, 0, BASSWASAPIInit.BASS_WASAPI_BUFFER, 1f, 0.05f, _WasapiProcess, IntPtr.Zero);
+
+            if (!result)
+            {
+                throw new Exception(Bass.BASS_ErrorGetCode().ToString());
+            }
+
+            BassWasapi.BASS_WASAPI_Start();
+            Thread.Sleep(500);
+        }
+
+        // Starts a new Analysis Thread
+        public void StartAnalysis()
+        {
+            // Kills currently running analysis thread if alive
+            if (_AnalysisThread != null && _AnalysisThread.IsAlive)
+            {
+                _AnalysisThread.Abort();
+            }
+
+            // Starts a new high-priority thread
+            _AnalysisThread = new Thread(new ThreadStart(this.ThreadProcSafe));
+
+            _AnalysisThread.Priority = ThreadPriority.Highest;
+            _AnalysisThread.Start();
+        }
+
+        // a thread-safe call on the TextBox control. 
+        private void ThreadProcSafe()
+        {
+            while (true)
+            {
+                Thread.Sleep(5);
+                var bands = TakeSample();
+                double bandSum = bands.Sum();
+                for (int i = 0; i < bands.Length; i++)
+                {
+                    double barPercent = bands[i] / bandSum * 100;
+                    ThreadHelperClass.UpdateProgressBar(this, bars[i], Convert.ToInt32(barPercent));
+                }
+                    
+            }
+
+        }
+
+        private double[] TakeSample()
+        {
+            int[] BandRange = { 4, 8, 18, 38, 48, 94, 140, 186, 466, 1022, 22000 };
+            double[] BandsTemp = new double[BANDS];
+            int n = 0;
+            float sum = 0;
+            int level = BassWasapi.BASS_WASAPI_GetLevel();
+            int ret = BassWasapi.BASS_WASAPI_GetData(_data, (int)BASSData.BASS_DATA_FFT1024); //get channel fft data
+            if (ret < -1) return null;
+            for (int i = 0; i < _data.Length; i++)
+            {
+                //Console.WriteLine("Frequency: " + i * _SamplingRate / 1024 + " value: " + _data[i]);
+                sum += _data[i];
+
+                if (i == BandRange[n])
+                {
+                    BandsTemp[n++] = (BANDS * sum) / 1024;
+                    Console.WriteLine("Band: " + BandRange[n] + " magnitude: " + sum);
+                    sum = 0;
+                }
+            }
+
+            return BandsTemp;
+        }
+
+        // WASAPI callback, required for continuous recording
+        private static int Process(IntPtr buffer, int length, IntPtr user)
+        {
+            return length;
+        }
+    }
+
+    public static class ThreadHelperClass
+    {
+        delegate void SetTextCallback(Form f, ProgressBar ctrl, int value);
+
+        public static void UpdateProgressBar(Form form, ProgressBar bar, int value)
+        {
+            // InvokeRequired required compares the thread ID of the 
+            // calling thread to the thread ID of the creating thread. 
+            // If these threads are different, it returns true. 
+            if (bar.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(UpdateProgressBar);
+                form.Invoke(d, new object[] { form, bar, value });
+            }
+            else
+            {
+                bar.Value = value;
+            }
+        }
+    }
+}
